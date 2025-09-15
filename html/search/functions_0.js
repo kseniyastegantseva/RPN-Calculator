71050281ecca6268b4ebd8061e2dc835f1f91c69
kseniyastegantseva <kseniyastegantseva@gmail.com>
2025-09-15 15:43:39 +0300

    Done

---
+++ html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['evaluate_5frpn_0',['evaluate_rpn',['../rpn_8h.html#ad2c611fc0925590d0db818b5571071e8',1,'evaluate_rpn(const std::string &amp;expression):&#160;rpn.cpp'],['../rpn_8cpp.html#a56be429255d682ade59a81e806b07752',1,'evaluate_rpn(const std::string &amp;expression):&#160;rpn.cpp']]]
+];
---
+++ include/rpn.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stack>
+#include <iostream>
+#include <string>
+
+/**
+ * @brief Evaluates a mathematical expression in Reverse Polish Notation (RPN).
+ *
+ * @param expression A string containing the RPN expression (e.g., "2 3 +").
+ * Tokens must be separated by spaces.
+ * @return stack The result of the evaluated expression.
+ * @exception std::invalid_argument Thrown if the expression is invalid,
+ * contains an unknown operator, or attempts division by zero.
+ */
+
+std::stack<double> evaluate_rpn(const std::string &expression);
